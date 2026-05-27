abf7171e1fe9c75d140dbb15f7fde0d11d64d756
Durjoy Sen <ayybosss69@gmail.com>
2026-05-27 01:38:32 -0400

    feat: add touch and undefsafe modules with documentation and examples
    - Added `to-regex-range` module with package.json.
    - Introduced `touch` module including README, LICENSE, and implementation files.
    - Implemented `undefsafe` module with example usage and tests.
    - Created a simple Express server with routes for home, about, contact, and weather data.

--- frontend/src/Pages/Page_weather.jsx
+++ frontend/src/Pages/Page_weather.jsx
@@ -3,6 +3,11 @@ import "./Page_weather.css"
 
 export default function Weather() {
     // Utilize API and useEffect to change weather
+    const [weather, setWeather] = useState("no value");
+    useEffect(() => {
+        fetch("http://localhost:3000/weather")
+            .then((response) => response.json())
+            .then((data) => setWeather(data));
+    }, []);
 
-    return <h1>test</h1>;
 }
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("CONTACT PAGE!!!!");
+});
+
+app.get("/weather", (req, res) => {
+  res.json([
+    {   
+        id: 1,
+        city: "New York",
+        temperature: 25,
+        condition: "Sunny"
+    },
+    {
+        id: 2,
+        city: "Los Angeles",
+        temperature: 30,
+        condition: "Cloudy"
+    }
+  ])
+});
+
+app.get("/weather/:id", (req, res) => {
+    const id = Number(req.params.id);
+    // Logic to find weather data by ID 
+
+    const weatherAPI = [
+        {
+          id: 1,
+          city: "New York",
+          temperature: 25,
+          condition: "Sunny",
+        },
+        {
+          id: 2,
+          city: "Los Angeles",
+          temperature: 30,
+          condition: "Cloudy",
+        },
+    ];
+
+    const findWeather = weatherAPI.find((weather) => weather.id === id);
+    res.json(findWeather);
+});
+
+app.listen(port, () => {
+  console.log("Hello World!!!!");
+});
